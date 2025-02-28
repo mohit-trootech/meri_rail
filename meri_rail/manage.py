b3d133ccefb774837b1af88b5498e108b2dc63a1
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-02-28 16:48:12 +0530

    [Update] Settings Path

--- meri_rail/manage.py
+++ meri_rail/manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meri_rail.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
